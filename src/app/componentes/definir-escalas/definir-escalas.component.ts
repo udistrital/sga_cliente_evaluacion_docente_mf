0962e71604a0a8d0627f0686c8378e2def6f44d0
EliSLopezM <emprendimientoeli@gmail.com>
2024-07-22 02:51:12 -0500

    feat:Creacion del nuevo componente para uso de escalafones udistrital/sga_documentacion#363

---
+++ src/app/componentes/definir-escalas/definir-escalas.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-definir-escalas',
+  standalone: true,
+  imports: [],
+  templateUrl: './definir-escalas.component.html',
+  styleUrl: './definir-escalas.component.scss'
+})
+export class DefinirEscalasComponent {
+
+}
