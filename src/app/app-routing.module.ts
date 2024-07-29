1ae2b075415db1a4a9851722a6122782609d0bab
josemasster <joseh.jgvr55@gmail.com>
2024-07-28 22:22:07 -0500

    se agrega funcionalidad de guards

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { getSingleSpaExtraProviders } from 'single-spa-angular';
 import { provideHttpClient, withFetch } from '@angular/common/http';
+import { AuthGuard } from 'src/_guards/auth.guard';
 // Importar el componente EmptyRouteComponent
 
 const routes: Routes = [
@@ -13,22 +14,27 @@ const routes: Routes = [
   // Otros componentes comentados porque no existen
   // {
   //   path: 'preasignacion',
+  //   canActivate: [AuthGuard],
   //   component: PreasignacionComponent,
   // },
   // {
   //   path: 'asignar',
+  //   canActivate: [AuthGuard],
   //   component: AsignarPtdComponent,
   // },
   // {
   //   path: 'verificar',
+  //   canActivate: [AuthGuard],
   //   component: VerificarPtdComponent,
   // },
   // {
   //   path: 'consolidado',
+  //   canActivate: [AuthGuard],
   //   component: ConsolidadoComponent,
   // },
   // {
   //   path: 'consolidado/revision',
+  //   canActivate: [AuthGuard],
   //   component: RevisionConsolidadoComponent,
   // },
   {
