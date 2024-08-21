9e51e0d7a344a2dc9251689b7780293276adaf76
Oscar Lozano <diazdaniel30@hotmail.com>
2024-08-21 13:43:34 -0500

    feat: Implementación de servicio y restricciones en ROLES udistrital/sga_documentacion#421

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,29 +14,20 @@ import { AppComponent } from './app.component';
 import { DefinicionFormulariosComponent } from './components/definicion-formularios/definicion-formularios.component';
 import { AsignacionFechasComponent } from './components/asignacion-fechas/asignacion-fechas.component';
 import { DefinirEscalasComponent } from './components/definir-escalas/definir-escalas.component';
-<<<<<<< HEAD
-import { EvaluacionesComponent } from './components/evaluaciones/evaluaciones.component';
-import { NgIsGrantedDirective } from './directives/ng-is-granted.directive';
-import { UserService } from './services/user.service';
-=======
->>>>>>> origin/develop
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatInputModule } from '@angular/material/input';
 import { MatNativeDateModule } from '@angular/material/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatFormFieldModule } from '@angular/material/form-field';
-<<<<<<< HEAD
-import { MatDialogModule } from '@angular/material/dialog';
-=======
 import { EvaluacionesComponent } from './components/evaluaciones/evaluaciones.component';
 import { NgIsGrantedDirective } from './directives/ng-is-granted.directive';
 import { UserService } from './services/user.service';
-import { HeteroevaluacionComponent } from './components/evaluaciones/heteroevaluacion/heteroevaluacion.component';
+/* import { HeteroevaluacionComponent } from './components/evaluaciones/heteroevaluacion/heteroevaluacion.component';
 import { AutoevaluacionIComponent } from './components/evaluaciones/autoevaluacion-i/autoevaluacion-i.component';
 import { CoevaluacionIComponent } from './components/evaluaciones/coevaluacion-i/coevaluacion-i.component';
 import { AutoevaluacionIIComponent } from './components/evaluaciones/autoevaluacion-ii/autoevaluacion-ii.component';
-import { CoevaluacionIIComponent } from './components/evaluaciones/coevaluacion-ii/coevaluacion-ii.component';
+import { CoevaluacionIIComponent } from './components/evaluaciones/coevaluacion-ii/coevaluacion-ii.component'; */
 import { MetricasComponent } from './components/metricas/metricas.component';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { MatCardModule } from '@angular/material/card';
@@ -50,7 +41,6 @@ import { MatStepperModule } from '@angular/material/stepper';
 import { MatTabsModule } from '@angular/material/tabs';
 import { NgxChartsModule } from '@swimlane/ngx-charts';
 import { ResultadosComponent } from './components/resultados/resultados.component';
->>>>>>> origin/develop
 
 export function createTranslateLoader(http: HttpClient) {
   return new TranslateHttpLoader(http, 'http://localhost:4218/assets/i18n/', '.json');
@@ -63,18 +53,14 @@ export function createTranslateLoader(http: HttpClient) {
     AsignacionFechasComponent,
     DefinirEscalasComponent,
     EvaluacionesComponent,
-<<<<<<< HEAD
-    NgIsGrantedDirective
-=======
-    NgIsGrantedDirective 
-    HeteroevaluacionComponent,
+    NgIsGrantedDirective,
+/*     HeteroevaluacionComponent,
     AutoevaluacionIComponent,
     CoevaluacionIComponent,
     AutoevaluacionIIComponent,
-    CoevaluacionIIComponent,
+    CoevaluacionIIComponent, */
     MetricasComponent, 
     ResultadosComponent
->>>>>>> origin/develop
   ],
   imports: [
     BrowserModule,
