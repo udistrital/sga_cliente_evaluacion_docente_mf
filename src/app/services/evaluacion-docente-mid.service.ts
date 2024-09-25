5bb2e40be4fb07b4e3a32da11ff879fca08f358c
josemasster <joseh.jgvr55@gmail.com>
2024-09-25 08:56:04 -0500

    Ajuste en el service de mid evaluacion docente

--- src/app/services/evaluacion-docente-mid.service.ts
+++ src/app/services/evaluacion-docente-mid.service.ts
@@ -8,26 +8,26 @@ import { RequestManager } from '../managers/requestManager';
 export class EvaluacionDocenteMidService {
 
   constructor(private requestManager: RequestManager) {
-    this.requestManager.setPath('SGA_EVALUACION_DOCENTE');
+    this.requestManager.setPath('SGA_PLAN_TRABAJO_DOCENTE_MID_SERVICE');
   }
 
   get(endpoint: any) {
-    this.requestManager.setPath('SGA_EVALUACION_DOCENTE');
+    this.requestManager.setPath('SGA_PLAN_TRABAJO_DOCENTE_MID_SERVICE');
     return this.requestManager.get(endpoint);
   }
 
   post(endpoint: any, element: any) {
-    this.requestManager.setPath('SGA_EVALUACION_DOCENTE');
+    this.requestManager.setPath('SGA_PLAN_TRABAJO_DOCENTE_MID_SERVICE');
     return this.requestManager.post(endpoint, element);
   }
 
   put(endpoint: any, element: any) {
-    this.requestManager.setPath('SGA_EVALUACION_DOCENTE');
+    this.requestManager.setPath('SGA_PLAN_TRABAJO_DOCENTE_MID_SERVICE');
     return this.requestManager.put(endpoint, element);
   }
 
   delete(endpoint: any, element: any) {
-    this.requestManager.setPath('SGA_EVALUACION_DOCENTE');
+    this.requestManager.setPath('SGA_PLAN_TRABAJO_DOCENTE_MID_SERVICE');
     return this.requestManager.delete(endpoint, element.Id);
   }
 }
